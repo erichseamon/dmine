a41e0f4266d590140ae72b9b6393f846d992f93d
Erich Seamon <erichs@uidaho.edu>
2016-03-26 23:19:59 -0700

    timelapse addition

--- agmesh_timelapse.R
+++ agmesh_timelapse.R
@@ -1,2 +1,5 @@
-setwd(paste("/agmesh-scenarios/", scen, "/ETtimelapse", sep="")) 
-system("ffmpeg -i '%*.jpg' -r 30 -q:v 2 timelapse.mp4")
+setwd("/git/data/USDA/jpgs/") 
+system("rm timelapse.mp4")
+setpts=2.0*PTS
+system("ffmpeg -i '%*.jpg' -r 30 -q:v 1 timelapse.mp4")
+system("ffmpeg -i timelapse.mp4 -vf "setpts=2.0*PTS" slowtimelapse.mp4")
