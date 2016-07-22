6eeb885f5bb241ce3e41ec3935bedfa29dc2e894
Erich Seamon <erichs@uidaho.edu>
2016-07-21 23:37:08 -0700

    updated models for commodities for python and R

--- agmesh-commodity-split.R
+++ agmesh-commodity-split.R
@@ -1,12 +1,13 @@
 library(rasterVis)
 library(data.table)
 
-setwd("/agmesh-scenarios/scenario_52177")
+setwd("/agmesh-scenarios/scenario_52177/summaries/")
 combined.df <- data.frame(read.csv("2001_2015_usda_gridmet_WA"))
 
 #-remove all other variables to allow for datasets based on year, month, county, and commodity - loss and acres
 
-combined.df <- subset(combined.df, select = -c(insuranceplancode,insurancename,stagecode,damagecausecode,damagecause,month,statecode,state,countyfips,countycode,bi,pr,th,pdsi,pet,erc,rmin,rmax,tmmn,tmmx,srad,sph,vs,fm1000,fm100) )
+#combined.df <- subset(combined.df, select = -c(insuranceplancode,insurancename,stagecode,damagecausecode,damagecause,month,statecode,state,countyfips,countycode,bi,pr,th,pdsi,pet,erc,rmin,rmax,tmmn,tmmx,srad,sph,vs,fm1000,fm100) )
+#combined.df <- subset(combined.df, select = -c(insuranceplancode,insurancename,stagecode,damagecausecode,damagecause,month,statecode,state,countyfips,countycode,bi,pr,th,pdsi,pet,erc,rmin,rmax,tmmn,tmmx,srad,sph,vs,fm1000,fm100) )
 
 #-convert to a data table
 
@@ -15,6 +16,17 @@ combined.df <- data.table(combined.df)
 #-order the columns by commodity, then year, month, and county
 
 combined.df <- combined.df[with(combined.df, order(commoditycode,year,monthcode,county)), ]
+setwd("/agmesh-scenarios/scenario_52177/commodity_csv/")
+commoditynames <- c(unique(combined.df$commodity))
+
+for (i in commoditynames) {
+ x <- subset(combined.df, commodity == i)
+ assign(x, paste(i, ".csv", sep=""))
+}
+  
+  #lapply(commoditynames, function(funct){write.csv(combined.df$commodity[[funct]], file = paste(funct, ".csv", sep = ""))})
+}
+#----
 
 #-sum the acres and loss columns for all common rows  This merges all rows that have the same values exept for acres and loss.  We sum those to create a geographic
 #-representation for each commodity - for each county, year, and month.  This will be use to convert to a raster for comparison to meterological data.
@@ -24,6 +36,8 @@ combined.df <- combined.df[, lapply(.SD, sum), by=list(year,county,commoditycode
 combined.yearmonth <- split(combined.df,list(combined.df$year,combined.df$monthcode, combined.df$commoditycode))
 setwd("/agmesh-scenarios/scenario_52177/month/")
 lapply(names(combined.yearmonth), function(funct){write.csv(combined.yearmonth[[funct]], file = paste(funct, ".csv", sep = ""))})
+
+
 
 #--bringing in county shapefile
 setwd("/nethome/erichs/counties/")
@@ -74,7 +88,7 @@ for (i in unique) {
 
 
 combined.split <- split(combined.df, combined.df$commodity)
-setwd("/home/git/data/USDA/commodity/")
+setwd("/home/git/data/USDA/crop_indemnity_commodity/")
 commodityspan = unique(combined.df$commodity)
 
 lapply(names(combined.split), function(funct){write.csv(combined.split[[funct]], file = paste(funct, ".csv", sep = ""))})
