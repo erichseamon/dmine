628f2c68c2663140ea8c6bac6cedc2d494bf4bbf
Erich Seamon <erichs@uidaho.edu>
2016-09-28 23:11:40 -0700

    updated gridmet

--- dmine-gridmet-monthly_rev3.R
+++ dmine-gridmet-monthly_rev3.R
@@ -181,6 +181,7 @@ for (i in yearspan) {
       rasterout <- crop(rasterout, counties)
       png(paste(dirname, "/", j, "_", k, "_", i, ".png", sep=""))
       plot(rasterout, main = paste0("Monthly Plot for: ", j, ", ", k, ", ", i, sep=""))
+      plot(counties)
       dev.off() 
       #rasterout <- t(rasterout)
       proj4string(rasterout) <- projection 
@@ -299,6 +300,10 @@ system("mkdir month")
 system("mkdir raster_commodity")
 system("mkdir raster_commodity_plots")
 system("mkdir gridmet_monthly_plots")
+systen("mkdir commodity_csv")
+system("mkdir month_positive")
+system("mkdir commodity_csv_agr")
+system("mkdir commodity_csv_agr_month")
 system("mkdir month_png")
 system("mv *summary ./summaries")
 system("mv *_monthly* ./summaries")
@@ -324,13 +329,13 @@ library(fields)
 library(dplyr)
 library(tidyr)
 
-setwd(paste("/dmine/data/USDA/agmesh-scenarios/", scen, "/summaries", sep=""))
-combined.df <- data.frame(read.csv(paste(N1, "_", N2, "_", "usda_gridmet_", scen_state, sep="")))
+setwd(paste("/dmine/data/USDA/agmesh-scenarios/", scen_state, "/summaries", sep=""))
+combined.df <- data.frame(read.csv(paste(N1, "_", N2, "_", "usda_gridmet_", scen_state, sep=""), strip.white = TRUE))
 
 #-remove all other variables to allow for datasets based on year, month, county, and commodity - loss and acres
 combined.df2 <- subset(combined.df, select = -c(insuranceplancode,insurancename,stagecode,damagecausecode,damagecause,month,statecode,state,countyfips,countycode,bi,pr,th,pdsi,pet,erc,rmin,rmax,tmmn,tmmx,srad,sph,vs,fm1000,fm100) )
 
-combined.df <- subset(combined.df, select = -c(insuranceplancode,insurancename,stagecode,damagecausecode,damagecause,month,statecode,state,countyfips,countycode,bi,pr,th,pdsi,pet,erc,rmin,rmax,tmmn,tmmx,srad,sph,vs,fm1000,fm100) )
+combined.df <- subset(combined.df, select = -c(insuranceplancode,insurancename,stagecode,damagecausecode,month,statecode,state,countyfips,countycode,bi,pr,th,pdsi,pet,erc,rmin,rmax,tmmn,tmmx,srad,sph,vs,fm1000,fm100) )
 
 #-convert to a data table
 
@@ -345,7 +350,8 @@ setwd(paste("/dmine/data/USDA/agmesh-scenarios/", "Idaho", "/commodity_csv/", se
 #lapply(combined.df$commodity, function(funct){sub("\\s+$", "", combined.df$commodity[[funct]])})
 commoditynames <- unique(combined.df$commodity)
 ttt <- function (x) sub("\\s+$", "", x)
-commoditytrim <- unique(ttt(combined.df$commodity))
+#commoditytrim <- unique(ttt(combined.df$commodity))
+commoditytrim <- unique(combined.df$commodity)
 
 for (i in commoditytrim) {
   x <- subset(combined.df, combined.df$commodity == i)
@@ -361,29 +367,48 @@ for (i in commoditytrim) {
 
 combined.df <- combined.df[with(combined.df, order(commoditycode,year,monthcode,county)), ]
 
+combined.df <- subset(combined.df, commoditycode != "NA")
+
 #-sum the acres and loss columns for all common rows  This merges all rows that have the same values exept for acres and loss.  We sum those to create a geographic
 #-representation for each commodity - for each county, year, and month.  This will be use to convert to a raster for comparison to meterological data.
 
-combined.df <- combined.df[, lapply(.SD, sum), by=list(year,county,commoditycode,monthcode)]
-
+combined.df <- combined.df[, lapply(.SD, sum), by=list(year,county,commoditycode,monthcode,commodity)]
+#combined.df <- data.frame(lapply(combined.df, trimws))
 #--replacing commoditycode with commodity name
 
-profession.code <- c(Apples=54, Wheat=11, Barley=91, SugarBeets=39, Cherries=57, Grapes=53, AdjustedGrossRevenue=63, 
-                     GreenPeas=64, AllOtherCrops=99, Pears=89, Canola=15, SweetCorn=42, Mint=74, Potatoes=84, 
-                     DryPeas=67, ProcessingBeans=46, DryBeans=47, Onions=13, Cranberries=58, Corn=41, 
-                     Oats=16, AlfalfaSeed=107, FreshApricots=218, FresFreestonePeaches=223, Nursery=73, 
-                     Mustard=69, Bluberries=12, AdjustedGrossRevenuelite=61, Plums=92, Soybeans=81, 
-                     WholeFarmRevenueProtection=76, Buckwheat=114)
+#profession.code <- c(Apples=54, Wheat=11, Barley=91, SugarBeets=39, Cherries=57, Grapes=53, AdjustedGrossRevenue=63, 
+#                     GreenPeas=64, AllOtherCrops=99, Pears=89, Canola=15, SweetCorn=42, Mint=74, Potatoes=84, 
+#                     DryPeas=67, ProcessingBeans=46, DryBeans=47, Onions=13, Cranberries=58, Corn=41, 
+#                     Oats=16, AlfalfaSeed=107, FreshApricots=218, FreshFreestonePeaches=223, Nursery=73, 
+#                     Mustard=69, Bluberries=12, AdjustedGrossRevenuelite=61, Plums=92, Soybeans=81, 
+#                     WholeFarmRevenueProtection=76, Buckwheat=114)
 
-combined.df$commodity <- names(profession.code)[match(combined.df$commoditycode, profession.code)]
+#combined.df$commodity <- names(profession.code)[match(combined.df$commoditycode, profession.code)]
 
 #-----
+combined.df$year <- as.numeric(as.character(combined.df$year))
+
+vect <- c(N1:N2)
+combined.df2 <- subset(combined.df, year > 2000) 
+combined.df3 <- subset(combined.df2, year < 2017) 
+
+combined.df <- combined.df3
 
 combined.yearmonth <- split(combined.df,list(combined.df$year,combined.df$monthcode, combined.df$commodity))
-setwd(paste("/dmine/data/USDA/agmesh-scenarios/", scen, "/month", sep=""))
+setwd(paste("/dmine/data/USDA/agmesh-scenarios/", scen_state, "/month", sep=""))
 lapply(names(combined.yearmonth), function(funct){write.csv(combined.yearmonth[[funct]], file = paste(funct, ".csv", sep = ""))})
 
-system("mv *AdjustedGrossRevenue.csv ../commodity_csv_agr/")
+system("mv *ADJUSTED* ../commodity_csv_agr/")
+system("rm *NA*")
+system("rm *..*")
+
+monthdir <- paste("/dmine/data/USDA/agmesh-scenarios/", scen_state, sep="")
+setwd(monthdir)
+system("find month -type f -size +100c -exec cp -nv {} month_positive/ \\;")
+
+
+#-------------------------------------------------
+
 
 #--bringing in county shapefile
 setwd("/dmine/data/counties/")
@@ -399,8 +424,8 @@ counties <- subset(counties, STATE_NAME %in% scen_state)
 monthdir <- paste("/dmine/data/USDA/agmesh-scenarios/", scen, sep="")
 #unique <- list.files(paste("/dmine/data/USDA/agmesh-scenarios/", scen, "/month", sep=""))
 maskraster <- raster(paste("/dmine/data/USDA/agmesh-scenarios/", scen, "/netcdf/pdsi_apr_", N2, ".nc", sep=""))
-setwd(monthdir)
-system("find month -type f -size +75c -exec cp -nv {} month_positive/ \\;")
+#setwd(monthdir)
+#system("find month -type f -size +75c -exec cp -nv {} month_positive/ \\;")
 
 setwd(paste("/dmine/data/USDA/agmesh-scenarios/", scen, "/month_positive/", sep=""))
 system("mv *AdjustedGrossRevenue.csv ../commodity_csv_agr_month/")
@@ -420,7 +445,7 @@ for (i in unique) {
   projection = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
   counties <- subset(counties, STATE_NAME %in% scen_state)
   #counties <- counties[grep(scen_state, counties@data$STATE_NAME),]
-  setwd(paste("/dmine/data/USDA/agmesh-scenarios/", scen, "/month", sep=""))
+  setwd(paste("/dmine/data/USDA/agmesh-scenarios/", scen_state, "/month", sep=""))
   x <- as.data.frame(read.csv(i, strip.white = TRUE))
   
   colnames(x) <- c("UNIQUEID", "YEAR", "COUNTY", "COMMODITYCODE", "MONTHCODE", "ACRES", "LOSS", "COMMODITY")
