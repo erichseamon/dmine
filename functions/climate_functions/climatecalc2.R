fd7fab067a184e59d608bcac17a3c6bbe0b58cce
Erich Seamon <erichs@uidaho.edu>
2017-03-15 23:53:18 -0700

    updates

---
+++ functions/climate_functions/climatecalc2.R
@@ -0,0 +1,133 @@
+
+climatecalc <- function(gfunction, statesclim, startdatec, enddatec) {
+
+  sourceDir("/home/git/dmine/functions/climate_functions")
+
+  library(dplyr)
+  library(tidyr)
+  library(geoknife) #order matters because 'query' is masked by a function in dplyr
+  library(RColorBrewer)
+  library(maps)
+  
+  statevar <- as.character(substitute(gfunction))
+  statevar <- substring(statevar, 4, 9)
+
+  months <- seq(from=as.Date(startdatec), to=as.Date(enddatec),by='months' )
+  monthspan <- length(months) - 1
+
+  
+  
+  final <- data.frame()
+  for (i in 1:monthspan) {
+    ii <- i+1
+    precipData <- gfunction(states = statesclim, 
+                            startDate = as.character(months[i]), 
+                            endDate = as.character(months[ii]))
+    
+    precipData$date <- months[i]
+    precipData$month <- months(as.Date(precipData$date))
+    
+    precipData$month <- substring(precipData$date,6,7)
+    precipData$month <- month.abb[as.numeric(precipData@data[8,8])]
+    precipData$month <- tolower(precipData$month)
+    
+    precipData$year <- substring(precipData$date,1,4)
+    final <- rbind(data.frame(precipData), final)
+    assign(paste(statevar, "final", sep=""), final)
+    
+  }
+  
+  return(get(paste(statevar, "final", sep="")))
+
+  
+}
+
+
+
+
+climatecalc <- function(statesclim, statevar, startdatec, enddatec) {
+  source("/home/git/dmine/functions/admin_functions/sourceDir.R")
+  sourceDir("/home/git/dmine/functions/climate_functions")
+  
+  library(dplyr)
+  library(tidyr)
+  library(geoknife) #order matters because 'query' is masked by a function in dplyr
+  library(RColorBrewer)
+  library(maps)
+  
+  months <- seq(from=as.Date(startdatec), to=as.Date(enddatec),by='months' )
+  monthspan <- length(months) - 1
+  
+  statevar2 <- as.character(statevar)
+  statevar2 <- substring(statevar2, 4, 7)
+  
+  
+  final <- data.frame()
+  for (i in 1:monthspan) {
+    ii <- i+1
+    precipData <- statevar(states = statesclim,
+                            startDate = as.character(months[i]),
+                            endDate = as.character(months[ii]))
+    
+    precipData$date <- months[i]
+    precipData$month <- months(as.Date(precipData$date))
+    
+    precipData$month <- substring(precipData$date,6,7)
+    precipData$month <- month.abb[as.numeric(precipData@data[8,8])]
+    precipData$month <- tolower(precipData$month)
+    
+    precipData$year <- substring(precipData$date,1,4)
+    final <- rbind(data.frame(precipData), final)
+    assign(paste(statevar2, "final", sep=""), final)
+    
+  }
+  
+  return(get(paste(statevar2, "final", sep="")))
+  
+  
+}
+
+
+climatecalc <- function(statevar, statesclim, startdatec, enddatec) {
+  source("/home/git/dmine/functions/admin_functions/sourceDir.R")
+  sourceDir("/home/git/dmine/functions/climate_functions")
+  
+  library(dplyr)
+  library(tidyr)
+  library(geoknife) #order matters because 'query' is masked by a function in dplyr
+  library(RColorBrewer)
+  library(maps)
+  
+  statevar2 <- paste("get", statevar, sep="")
+  gfunction <- eval(parse(text = statevar2))
+  
+  months <- seq(from=as.Date(startdatec), to=as.Date(enddatec),by='months' )
+  monthspan <- length(months) - 1
+  
+  
+  
+  final <- data.frame()
+  for (i in 1:monthspan) {
+    ii <- i+1
+    precipData <- gfunction(states = statesclim,
+                            startDate = as.character(months[i]),
+                            endDate = as.character(months[ii]))
+    
+    precipData$date <- months[i]
+    precipData$month <- months(as.Date(precipData$date))
+    
+    precipData$month <- substring(precipData$date,6,7)
+    precipData$month <- month.abb[as.numeric(precipData@data[8,8])]
+    precipData$month <- tolower(precipData$month)
+    
+    precipData$year <- substring(precipData$date,1,4)
+    final <- rbind(data.frame(precipData), final)
+    assign(paste(statevar2, "final", sep=""), final)
+    
+  }
+  
+  return(get(paste(statevar2, "final", sep="")))
+  
+  
+}
+
--- functions/climate_functions/metaclimatecalc.R
+++ functions/climate_functions/metaclimatecalc.R
@@ -1,5 +1,21 @@
 metaclimatecalc <- function(metastates, metastart, metaend) {
 
-climatecalc(getPDSI, metastates, metastart, metaend)
-  
+#climatecalc(getPDSI, metastates, "PDSI", metastart, metaend)
+climatecalc(getPR, metastates, "PR", metastart, metaend)
+#climatecalc(getTMMN, metastates, metastart, metaend)
+#climatecalc(getTMMX, metastates, metastart, metaend)
+#climatecalc(getRMIN, metastates, metastart, metaend)
+#climatecalc(getRMAX, metastates, metastart, metaend)
+#climatecalc(getSRAD, metastates, metastart, metaend)
+#climatecalc(getSPH, metastates, metastart, metaend)
+#climatecalc(getPET, metastates, metastart, metaend)
+#climatecalc(getERC, metastates, metastart, metaend)
+#climatecalc(getVS, metastates, metastart, metaend)
+#climatecalc(getTH, metastates, metastart, metaend)
+#climatecalc(getBI, metastates, metastart, metaend)
+#climatecalc(getFM100, metastates, metastart, metaend)
+#climatecalc(getFM1000, metastates, metastart, metaend)
+#finalfinal <- cbind(PDSIfinal[6], PRfinal[6], PDSIfinal[1:5], PDSIfinal[8:9])
+#return(finalfinal)
 }
+
